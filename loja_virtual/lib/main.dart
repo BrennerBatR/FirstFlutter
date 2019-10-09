ae61f0dc5744908c1842dc4dff6e305999571b89
Brenner Batista <brenner.batista@clubpetro.com.br>
2019-10-09 15:40:53 -0300

    Drawer

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -1,23 +1,18 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:loja_virtual/screens/home_screen.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter's Clothing",
+      title: "Flutter's Shoes",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-          primaryColor: Colors.red
-
-         // primaryColor: Color.fromARGB(255, 4, 125, 141)
-      ),
+          primarySwatch: Colors.blue,
+          primaryColor: Color.fromARGB(255, 4, 125, 141)),
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
-
   }
 }
